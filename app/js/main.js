de3445c537590992815b52e0c199dd4f550e4b1e
webdeveloperjun <maks.lesko35@gmail.com>
2022-04-12 10:53:02 +0300

    video fashion

--- app/js/main.js
+++ app/js/main.js
@@ -1,6 +1,12 @@
 
 $(function(){
 
-  $('slider-inner').slick()
+  $('.slider-inner').slick({
+    dots: true,
+    arrows: false,
+    fade: true,
+    autoplay: true,
+    autoplaySpeed: 3000
+  });
 
 });
